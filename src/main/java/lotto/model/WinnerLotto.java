b94b517f221b2494d8a616aec379b82036f047c3
jongheonleee <qwefghnm1212@gmail.com>
2023-12-09 17:42:43 +0900

    [feat] WinnerLotto 클래스 생성 - 결과 계산 기능 추후 업데이트

--- src/main/java/lotto/model/WinnerLotto.java
+++ src/main/java/lotto/model/WinnerLotto.java
@@ -1,5 +1,84 @@
 package lotto.model;
 
-public class WinnerLotto {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
 
+public class WinnerLotto {
+    private static final Integer START_RANGE = 1;
+    private static final Integer END_RANGE = 45;
+    private static final Integer LENGTH = 6;
+    private final List<Integer> winnerNumbers;
+    private final Integer bonusNumber;
+
+    public WinnerLotto(List<Integer> winnerNumbers, Integer bonusNumber) {
+        validate(winnerNumbers, bonusNumber);
+        this.winnerNumbers = new ArrayList<>(winnerNumbers);
+        Collections.sort(this.winnerNumbers);
+        this.bonusNumber = bonusNumber;
+    }
+
+    private void validate(List<Integer> numbers, Integer bonusNumber) {
+        if (!isAllowedNumbers(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호 허용범위는 1~45");
+        }
+
+        if (!isAllowedNumbersLength(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호 개수는 6개 입니다.");
+        }
+
+        if (isDuplicatedNumber(numbers)) {
+            throw new IllegalArgumentException("[ERROR] 중복된 숫자가 있습니다.");
+        }
+
+        if (!isAllowedNumber(bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 로또 번호 허용범위는 1~45");
+        }
+
+        if (isDuplicatedBetweenWinnerNumbersAndBonusNumber(numbers, bonusNumber)) {
+            throw new IllegalArgumentException("[ERROR] 중복된 숫자가 있습니다.");
+        }
+    }
+
+    private Boolean isAllowedNumbers(List<Integer> numbers) {
+        return numbers.stream()
+                .allMatch(number -> isAllowedNumber(number));
+    }
+
+    private Boolean isAllowedNumber(Integer number) {
+        return START_RANGE <= number && number <= END_RANGE;
+    }
+
+    private Boolean isAllowedNumbersLength(List<Integer> numbers) {
+        return numbers.size() == LENGTH;
+    }
+
+    private Boolean isDuplicatedNumber(List<Integer> numbers) {
+        return numbers.stream()
+                .collect(Collectors.toSet())
+                .size() != LENGTH;
+    }
+
+
+    private Boolean isDuplicatedBetweenWinnerNumbersAndBonusNumber(List<Integer> numbers, Integer bonusNumber) {
+        return winnerNumbers.contains(bonusNumber);
+    }
+
+    private Integer countHitNumber(List<Integer> numbers) {
+        return numbers.stream()
+                .filter(number -> winnerNumbers.contains(number))
+                .collect(Collectors.toList())
+                .size();
+    }
+
+    private Boolean isHitBonusNumber(List<Integer> numbers) {
+        return numbers.stream()
+                .anyMatch(number -> number == bonusNumber);
+    }
+
+//    이 부분 추후에 업데이트
+//    public LottoResult calculateLottoResult(List<Integer> numbers) {
+//        return
+//    }
 }
