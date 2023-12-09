397689ce7a8c0fdd8e51a29264df1f024c18c078
jongheonleee <qwefghnm1212@gmail.com>
2023-12-09 15:37:20 +0900

    [feat] RandomGenerator 클래스 생성

--- src/main/java/lotto/util/RandomGenerator.java
+++ src/main/java/lotto/util/RandomGenerator.java
@@ -1,5 +1,15 @@
 package lotto.util;
 
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.List;
+
 public class RandomGenerator {
 
+    private static final Integer START_RANGE = 1;
+    private static final Integer END_RANGE = 45;
+    private static final Integer LOTTO_LENGTH = 6;
+
+    public static List<Integer> generateNumbers() {
+        return Randoms.pickUniqueNumbersInRange(START_RANGE, END_RANGE, LOTTO_LENGTH);
+    }
 }
